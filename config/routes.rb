551b91375317d891a93cad985bb3a6b0d88da146
Brian Truong <briantruong@berkeley.edu>
2015-03-12 15:15:27 -0700

    test: should commit from my main github

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Decal::Application.routes.draw do
-  
-  root :to => 'courses#index'
 
   resources :courses
+  
+  root :to => 'courses#index'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
