3ba2155414bec3f65e1a16218d3e60a64eb0ff23
Felix Liu <felixliu@berkeley.edu>
2015-04-02 21:30:17 -0700

    strongly typed

--- app/controllers/existing_courses_controller.rb
+++ app/controllers/existing_courses_controller.rb
@@ -3,7 +3,7 @@ class ExistingCoursesController < ApplicationController
 	    if current_user.nil? || !(user_signed_in?)
 	    	redirect_to :root, notice: 'make sure you login fool'
 	    else
-			@courses = Course.where("uid is not ?", current_user.id)
+			@courses = Course.where("uid is not ?", current_user.id.to_s)
 	    end
 	end
 
@@ -13,7 +13,7 @@ class ExistingCoursesController < ApplicationController
 			if params[key] == "1"
 				# test.push(key)
 				course = Course.find_by_title(key)
-				course.update_attributes(uid: current_user.id)
+				course.update_attributes(uid: current_user.id.to_s)
 			end
 		end
 		redirect_to dashboard_index_path
