ef8cb41961d711aa7d7eaab0894f2d144172cbe8
Felix Liu <felixliu@berkeley.edu>
2015-04-02 22:14:47 -0700

    hopefully this time, the fucking uid changes

--- db/migrate/20150401062147_add_user_id_to_courses.rb
+++ db/migrate/20150401062147_add_user_id_to_courses.rb
@@ -1,5 +1,5 @@
 class AddUserIdToCourses < ActiveRecord::Migration
   def change
-  	add_column :courses, :uid, :string
+  	add_column :courses, :uid, :integer
   end
 end
--- db/migrate/20150403050341_remove_and_add_uid.rb
+++
@@ -1,6 +0,0 @@
-class RemoveAndAddUid < ActiveRecord::Migration
-  def change
-  	remove_column :courses, :uid, :string
-  	add_column :courses, :uid, :integer
-  end
-end
