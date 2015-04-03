0c9205e3dcd5c735f96b8dc78f09b42b36674fd1
Ted Xiao <t.xiao@berkeley.edu>
2015-04-02 21:43:42 -0300

    removed colon from css

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      'AKIAINJSHKF4JJYCKSCA',
-    aws_secret_access_key:  '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',
+    aws_access_key_id:      ENV['S3_KEY'],
+    aws_secret_access_key:  ENV['S3_SECRET'],
   }
 
   # For testing, upload files to local `tmp` folder.
@@ -15,5 +15,5 @@ CarrierWave.configure do |config|
   end
 
   config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
-  config.fog_directory    = 'decal-169'
+  config.fog_directory    = ENV['S3_BUCKET']
 end
