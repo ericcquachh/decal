2bdb8946caf77b61c758477e479a0026b8157974
Felix Liu <felixliu@berkeley.edu>
2015-04-08 14:49:29 -0700

    issue with removing columns

--- db/migrate/20150310220455_create_courses.rb
+++ db/migrate/20150310220455_create_courses.rb
@@ -6,11 +6,8 @@ class CreateCourses < ActiveRecord::Migration
       t.string :units
       t.string :time
       t.string :status
-
       t.timestamps
     end
 
-    remove_column :courses, :days
-    remove_column :courses, :time
   end
 end
