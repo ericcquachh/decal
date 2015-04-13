610b1eced375f1486559b9b228a49059bbfc7045
Ted Xiao <t.xiao@berkeley.edu>
2015-04-12 23:05:22 -0300

    I TINK IT WURK

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     provider:               'AWS',
     aws_access_key_id:      ENV['S3_KEY'],
     aws_secret_access_key:  ENV['S3_SECRET'],
-    region:                 'us-west-2'
+    region:                 'us-west-1',
+    path_style:             true
   }
 
   # For testing, upload files to local `tmp` folder.
