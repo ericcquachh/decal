a99e88da01666246b28aa301fe4e4e3c2c5e8dcb
Brian Truong <briantruong@berkeley.edu>
2015-04-15 13:44:01 -0700

    test

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module Decal
     config.assets.version = '1.0'
 
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w(ckeditor/*)
+    # config.assets.precompile += Ckeditor.assets
+    # config.assets.precompile += %w(ckeditor/*)
   end
 end
