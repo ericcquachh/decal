5364b16e8c66602bf652a8eeeadd0a40ca34c8ba
Ted Xiao <t.xiao@berkeley.edu>
2015-04-16 00:23:42 -0300

    deleted random shit that rails added

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '2db5fa0afefd2c9f95edd41fe26a7017c7474eff51e13742b6dfeaaee1337b7f4f94b58a00fbc52ff454cc6ded836424c7bf0c913357c5444763d9651f48d10e';
