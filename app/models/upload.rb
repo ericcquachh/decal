ed43dae122b81d465a9ae8abafeb8b015437288e
Ted Xiao <t.xiao@berkeley.edu>
2015-04-27 00:11:36 -0300

    added uploads validations

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,4 +3,5 @@ class Upload < ActiveRecord::Base
   belongs_to :course, :class_name => "Course", :foreign_key => "course_id"
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
   validates :name, presence: true # Make sure the owner's name is present.
+  validates :attachment, presence: true
 end
